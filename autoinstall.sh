fefdbdd1f15d0960ddd2e7c5fb7b4d546953360e
Helielson <helielson@jusbrasil.com.br>
2014-05-16 14:03:41 -0300

    Updates installation script

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,22 +1,29 @@
-printf "[ \033[00;34mConfigurando...\033[0m ] $1\n"
+#!/bin/sh
 
-# 1. Checks if exists .vim and .vim/bundle
+INSTALLATION_DIR=~/workspace
 
-# 2 If exists, create a backup file of .vim
+printf "\033[2K\033[00;32mCreating a backup file of your vim file...\n\033[0m\n"
+
 mv ~/.vimrc ~/.vimrc.bak
 
-# 3 Creates a symbolic link for .vimrc file to vimrc file
+printf "\033[2K\033[00;32mBackup created in the ~/.vimrc.bac file\033[0m\n\n"
+
+printf "\033[2K\033[00;32mDownloading project...\033[0m\n\n"
+cd $INSTALLATION_DIR
+git clone git@github.com:helielson/vim-config.git
+cd vim-config
+
 CURRENT_DIR=`pwd`
 ln -s $CURRENT_DIR/vim ~/.vim
 ln -s $CURRENT_DIR/vim/vimrc ~/.vimrc
 
-printf "[ \033[00;34mInstalling plugins...\033[0m ] $1\n\n"
+printf "\033[2K\033[00;32mInstalling vim plugins...\033[0m\n\n"
 git submodule update --init
 
-printf "[ \033[00;34mInstalling Jedi lib (https://github.com/davidhalter/jedi)...\033[0m ] $1\n\n"
+printf "\033[2K\033[00;32mInstalling Jedi lib (https://github.com/davidhalter/jedi)...\033[0m\n\n"
 pip install jedi
 
-printf "[ \033[00;34mInstalling flake8 lib (https://github.com/singingwolfboy/flake8)...\033[0m ] $1\n\n"
+printf "\033[2K\033[00;32mInstalling flake8 lib (https://github.com/singingwolfboy/flake8)...\033[0m\n\n"
 pip install flake8
 
-printf "\r\033[2K[ \033[00;32mInstallation finished. Enjoy your vim :)\033[0m ] $1\n"
+printf "\n\r\033[2K\033[00;32mInstallation finished.\nEnjoy your vim :)\033[0m\n"
